92bca41a367c46486de26e04e81525e5039ccb19
MakovAl <sashamakov20.99@gmail.com>
2022-10-24 00:07:48 +0700

    Added logging and documentation. Refactoring in the onCreate method

--- app/src/main/java/com/makoval/mycalc/MainActivity.java
+++ app/src/main/java/com/makoval/mycalc/MainActivity.java
@@ -3,45 +3,24 @@ package com.makoval.mycalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
 
+/**
+ * Главный класс прложения калькулятор
+ */
 public class MainActivity extends AppCompatActivity {
 
-    private String calc(String textCalcScreen) {
-        String[] operands;
-        String stringResult = null;
-        if (textCalcScreen.contains("*")) {
-            operands = textCalcScreen.split("\\*");
-            int result = Integer.parseInt(operands[0]) * Integer.parseInt(operands[1]);
-            stringResult = String.valueOf(result);
-            
-        }
-        if (textCalcScreen.contains("/")) {
-            operands = textCalcScreen.split("/");
-            double result = Double.parseDouble(operands[0]) / Double.parseDouble(operands[1]);
-            stringResult = String.valueOf(result);
-        }
-        if (textCalcScreen.contains("+")) {
-            operands = textCalcScreen.split("\\+");
-            int result = Integer.parseInt(operands[0]) + Integer.parseInt(operands[1]);
-            stringResult = String.valueOf(result);
-        }
-        if (textCalcScreen.contains("-")) {
-            operands = textCalcScreen.split("-");
-            int result = Integer.parseInt(operands[0]) - Integer.parseInt(operands[1]);
-            stringResult = String.valueOf(result);
-        }
-        return stringResult;
-    }
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    /**
+     * Метод получает кнопки с которыми может взаимодействовать пользователь, задает поведение
+     * при нажатие на них, в случае нажатия на кнопку с цифрами или оператором к строке выражения
+     * добавляется симовл
+     * соответсвующий кнопке.
+     */
+    private void setOnClickListeners() {
 
         TextView calcScreen = (TextView) findViewById(R.id.calc_screen);
         Button buttonOne = (Button) findViewById(R.id.button_one);
@@ -61,29 +40,185 @@ public class MainActivity extends AppCompatActivity {
         Button buttonDelete = (Button) findViewById(R.id.button_delete);
         Button buttonEqual = (Button) findViewById(R.id.button_equal);
 
-        buttonOne.setOnClickListener(view -> calcScreen.setText(calcScreen.getText() + "1"));
-        buttonTwo.setOnClickListener(view -> calcScreen.setText(calcScreen.getText() + "2"));
-        buttonThree.setOnClickListener(view -> calcScreen.setText(calcScreen.getText() + "3"));
-        buttonFour.setOnClickListener(view -> calcScreen.setText(calcScreen.getText() + "4"));
-        buttonFive.setOnClickListener(view -> calcScreen.setText(calcScreen.getText() + "5"));
-        buttonSix.setOnClickListener(view -> calcScreen.setText(calcScreen.getText() + "6"));
-        buttonSeven.setOnClickListener(view -> calcScreen.setText(calcScreen.getText() + "7"));
-        buttonEight.setOnClickListener(view -> calcScreen.setText(calcScreen.getText() + "8"));
-        buttonNine.setOnClickListener(view -> calcScreen.setText(calcScreen.getText() + "9"));
-        buttonZero.setOnClickListener(view -> calcScreen.setText(calcScreen.getText() + "0"));
-        buttonPlus.setOnClickListener(view -> calcScreen.setText(calcScreen.getText() + "+"));
-        buttonMinus.setOnClickListener(view -> calcScreen.setText(calcScreen.getText() + "-"));
-        buttonMul.setOnClickListener(view -> calcScreen.setText(calcScreen.getText() + "*"));
-        buttonDiv.setOnClickListener(view -> calcScreen.setText(calcScreen.getText() + "/"));
+
+        buttonOne.setOnClickListener(view -> {
+            calcScreen.setText(calcScreen.getText() + "1");
+            Log.i("MyCalc", "Пользователь нажал кнопку '1' ");
+        });
+        buttonTwo.setOnClickListener(view -> {
+            calcScreen.setText(calcScreen.getText() + "2");
+            Log.i("MyCalc", "Пользователь нажал кнопку '2' ");
+        });
+        buttonThree.setOnClickListener(view -> {
+            calcScreen.setText(calcScreen.getText() + "3");
+            Log.i("MyCalc", "Пользователь нажал кнопку '3' ");
+        });
+        buttonFour.setOnClickListener(view -> {
+            calcScreen.setText(calcScreen.getText() + "4");
+            Log.i("MyCalc", "Пользователь нажал кнопку '4' ");
+        });
+        buttonFive.setOnClickListener(view -> {
+            calcScreen.setText(calcScreen.getText() + "5");
+            Log.i("MyCalc", "Пользователь нажал кнопку '5' ");
+        });
+        buttonSix.setOnClickListener(view -> {
+            calcScreen.setText(calcScreen.getText() + "6");
+            Log.i("MyCalc", "Пользователь нажал кнопку '6' ");
+        });
+        buttonSeven.setOnClickListener(view -> {
+            calcScreen.setText(calcScreen.getText() + "7");
+            Log.i("MyCalc", "Пользователь нажал кнопку '7' ");
+        });
+        buttonEight.setOnClickListener(view -> {
+            calcScreen.setText(calcScreen.getText() + "8");
+            Log.i("MyCalc", "Пользователь нажал кнопку '8' ");
+        });
+        buttonNine.setOnClickListener(view -> {
+            calcScreen.setText(calcScreen.getText() + "9");
+            Log.i("MyCalc", "Пользователь нажал кнопку '9' ");
+        });
+        buttonZero.setOnClickListener(view -> {
+            calcScreen.setText(calcScreen.getText() + "0");
+            Log.i("MyCalc", "Пользователь нажал кнопку '0' ");
+        });
+        buttonPlus.setOnClickListener(view -> {
+            calcScreen.setText(calcScreen.getText() + "+");
+            Log.i("MyCalc", "Пользователь нажал кнопку '+' ");
+        });
+        buttonMinus.setOnClickListener(view -> {
+            calcScreen.setText(calcScreen.getText() + "-");
+            Log.i("MyCalc", "Пользователь нажал кнопку '-' ");
+        });
+        buttonMul.setOnClickListener(view -> {
+            calcScreen.setText(calcScreen.getText() + "*");
+            Log.i("MyCalc", "Пользователь нажал кнопку '*' ");
+        });
+        buttonDiv.setOnClickListener(view -> {
+            calcScreen.setText(calcScreen.getText() + "/");
+            Log.i("MyCalc", "Пользователь нажал кнопку '/' ");
+        });
         buttonDelete.setOnClickListener(view -> {
-            String textScreen = calcScreen.getText().toString();
-            calcScreen.setText(textScreen.substring(0, textScreen.length() - 1));
+            try {
+                Log.i("MyCalc", "Пользователь нажал кнопку '--' ");
+                String textScreen = calcScreen.getText().toString();
+                calcScreen.setText(textScreen.substring(0, textScreen.length() - 1));
+            } catch (StringIndexOutOfBoundsException ex) {
+                Log.e("MyCalc", ex.getMessage());
+            }
         });
         buttonEqual.setOnClickListener(view -> {
+            Log.i("MyCalc", "Пользователь нажал кнопку '=' ");
             String textScreen = calcScreen.getText().toString();
             calcScreen.setText(calc(textScreen));
         });
+    }
+
+    /**
+     * Метод выполняющий действия:
+     * Сложение, вычитание, умножение, деление.
+     * <p>
+     * Метод получает на вход строку содержащию выражение, в зависимости от находящегося в ней
+     * оператора выполняется соотвествущее этому оператору действие.
+     *
+     * @param textCalcScreen
+     * @return Результат вычисления
+     */
+    private String calc(String textCalcScreen) {
+        String[] operands;
+        String stringResult = null;
+
+        if (textCalcScreen.contains("*")) {
+            operands = textCalcScreen.split("\\*");
+            int result = Integer.parseInt(operands[0]) * Integer.parseInt(operands[1]);
+            stringResult = String.valueOf(result);
+
+        }
+        if (textCalcScreen.contains("/")) {
+            operands = textCalcScreen.split("/");
+            double result = Double.parseDouble(operands[0]) / Double.parseDouble(operands[1]);
+            stringResult = String.valueOf(result);
+        }
+        if (textCalcScreen.contains("+")) {
+            operands = textCalcScreen.split("\\+");
+            int result = Integer.parseInt(operands[0]) + Integer.parseInt(operands[1]);
+            stringResult = String.valueOf(result);
+        }
+        if (textCalcScreen.contains("-")) {
+            operands = textCalcScreen.split("-");
+            int result = Integer.parseInt(operands[0]) - Integer.parseInt(operands[1]);
+            stringResult = String.valueOf(result);
+        }
+        Log.i("MyCalc", "Результат: " + textCalcScreen + " = " + stringResult);
+        return stringResult;
+    }
 
 
+    /**
+     * Метод выполняющий действия при запуске приложения.
+     * <p>
+     * В данном случае установливает активность, которая будет отображаться на экране
+     *
+     * @param savedInstanceState
+     * @see this#calc(String)
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d("MyCalc", "Вызов onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("MyCalc", "Тест Info");
+        Log.e("MyCalc", "Тест Error");
+        Log.wtf("MyCalc", "Тест wtf");
+        Log.d("MyCalc", "Тест Debug");
+
+        setOnClickListeners();
+
+    }
+
+    /**
+     * Метод выполняющий действия промежуточного состояния между загрузкой и готовностью
+     * взаимодействовать с пользователем
+     */
+    @Override
+    protected void onResume() {
+        Log.d("MyCalc", "Вызов onResume");
+        super.onResume();
+    }
+
+    /**
+     * Метод выполняющий действия при восстановлении приложения из состояния Stopped
+     */
+    @Override
+    protected void onRestart() {
+        Log.d("MyCalc", "Вызов onRestart");
+        super.onRestart();
+    }
+
+    /**
+     * Метод выполняющий действия перед выходом приложения на передний план
+     */
+    @Override
+    protected void onStart() {
+        Log.d("MyCalc", "Вызов onStart");
+        super.onStart();
+    }
+
+    /**
+     * Метод выполняющий действия при завершении приложения
+     */
+    @Override
+    protected void onDestroy() {
+        Log.d("MyCalc", "Вызов onDestroy");
+        super.onDestroy();
+    }
+
+    /**
+     * Метод выполняющий действия при остановке приложения (например при сворачивании)
+     */
+    @Override
+    protected void onStop() {
+        Log.d("MyCalc", "Вызов onStop");
+        super.onStop();
     }
 }
